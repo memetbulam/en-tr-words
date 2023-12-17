f27e2e57ba7174000e6f7faeaa6577a0f65424d9
Mehmet Bulam <mehmet3bulam@gmail.com>
2023-12-17 19:25:26 +0300

    feat: word animation and text completed.

---
+++ src/utils/theme.ts
@@ -0,0 +1,13 @@
+import { keyframes } from "@emotion/react";
+
+export const rotateWawe = keyframes`
+0% {
+  transform: rotateY(0deg);
+}
+50%{
+    transform: rotateY(90deg);
+}
+100% {
+  transform: rotateY(0deg);
+}
+`;
---
+++ src/utils/types.ts
@@ -0,0 +1,6 @@
+export type Words = {
+  id: number;
+  en: string;
+  tr: string;
+  isTrWordViewed: boolean;
+};
